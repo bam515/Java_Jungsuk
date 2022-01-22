78546a7f064b358424879659d864fc568c684ff2
bam515 <imsb8415@gmail.com>
2022-01-22 22:23:49 +0900

    chapter09 ing

--- src/com/bam/chapter09/Ex9_1.java
+++ src/com/bam/chapter09/Ex9_1.java
@@ -1,5 +1,26 @@
 package com.bam.chapter09;
 
 public class Ex9_1 {
+	
+	public static void main(String[] args) {
+		
+		Value v1 = new Value(10);
+		Value v2 = new Value(10);
+		
+		if (v1.equals(v2))
+			System.out.println("v1과 v2는 같습니다.");
+		else 
+			System.out.println("v1과 v2는 다릅니다.");
+		
+	} // end of main method
 
+}
+
+class Value {
+	
+	int value;
+	
+	Value(int value) {
+		this.value = value;
+	}
 }
